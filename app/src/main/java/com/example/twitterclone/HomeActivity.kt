bb4e49a127d8811b8531dc670daff7deb332e060
Chevin <chloejeon2004@gmail.com>
2023-09-14 18:21:00 -0400

    start with HomeActivity

--- app/src/main/java/com/example/twitterclone/HomeActivity.kt
+++ app/src/main/java/com/example/twitterclone/HomeActivity.kt
@@ -1,5 +1,7 @@
 package com.example.twitterclone
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,9 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, HomeActivity::class.java)
     }
 }
