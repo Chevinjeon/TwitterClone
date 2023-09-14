4b8597792120b9a2c6acffcc6a6aa3fc25936091
Chevin <chloejeon2004@gmail.com>
2023-09-14 18:00:03 -0400

    added Nested Scrolling effect

--- app/src/main/java/com/example/twitterclone/LoginActivity.kt
+++ app/src/main/java/com/example/twitterclone/LoginActivity.kt
@@ -2,10 +2,19 @@ package com.example.twitterclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun onLogin(v: View){
+
+    }
+
+    fun goToSignup(v: View) {
+
     }
 }
