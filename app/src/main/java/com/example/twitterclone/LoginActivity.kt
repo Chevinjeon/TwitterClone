829936a13ea49aef3fa65b66299ccfc0070d8302
Chevin <chloejeon2004@gmail.com>
2023-09-14 18:41:41 -0400

    debugged Login functionality

--- app/src/main/java/com/example/twitterclone/LoginActivity.kt
+++ app/src/main/java/com/example/twitterclone/LoginActivity.kt
@@ -2,8 +2,12 @@ package com.example.twitterclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -13,13 +17,37 @@ class LoginActivity : AppCompatActivity() {
     private val firebaseAuthListener = FirebaseAuth.AuthStateListener {
         val user = firebaseAuth.currentUser?.uid
         user?.let {
-
+            startActivity(HomeActivity.newIntent(this))
+            finish()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setTextChangeListener(emailET, emailTIL)
+        setTextChangeListener(passwordET, passwordTIL)
+
+        loginProgressLayout.setOnTouchListener { v, event -> true }
+
+    }
+
+    fun setTextChangeListener(et: EditText, til: TextInputLayout) {
+        et.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                til.isErrorEnabled = false
+            }
+
+        })
+
     }
 
     // called when button is clicked
@@ -60,5 +88,13 @@ class LoginActivity : AppCompatActivity() {
 
     fun goToSignup(v: View) {
 
+    }
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth.addAuthStateListener (firebaseAuthListener)
+    }
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener)
     }
 }
